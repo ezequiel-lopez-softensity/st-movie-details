ecff118a3ba7afe1db942f6d0237e26fe49d4772
Ezequiel López <ezequiel.lopez@softensity.com>
2022-12-26 16:16:51 -0300

    feat: solution created

---
+++ st-movie-details/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace st_movie_details;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ st-movie-details/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace st_movie_details;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
